8c978d4f8b87681ba51175f4195998411a9aefc5
taitan-x <138150134+taitan-x@users.noreply.github.com>
2023-08-23 11:27:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Taitan-x/VIPMUSIC",
+    "https://github.com/taitan-x/VIPMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
